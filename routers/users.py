084ef23f3780e651d87b40d05f7e03e49d4c6efe
Stags <stags@isdino.com>
2024-05-03 18:35:12 +0000

    first commit

---
+++ routers/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends, HTTPException
+
+from ..dependencies import get_query_token, get_token_header
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+@router.get("/")
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
