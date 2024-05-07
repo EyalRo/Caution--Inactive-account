6876d0692541c862373064c06606e83fb943d49c
Stags <stags@isdino.com>
2024-05-07 06:16:24 +0000

    login function taking shape

--- data/crud.py
+++ data/crud.py
@@ -1,23 +1,30 @@
-import couchdb
-from . import models, schemas
+import couchdb, os
+from . import schemas
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+from dotenv import load_dotenv
 
+load_dotenv()
 
-def get_user_by_email(email: str):
-    return ()
+DB_STRING = os.getenv("DB_STRING")
+
+if DB_STRING:
+    couch = couchdb.Server(DB_STRING)
+    db = couch["inactive-account"]
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_user_by_email_and_password(email: str, password: str):
-    hashed_password = password
+    hashed_password = get_password_hash(password)
+    mango = {"selector": {"email_address": email, "password_hash": hashed_password}}
+    db.find(mango)
     return ()
 
 
-def get_users(skip: int = 0, limit: int = 100):
-    return ()
-
-
-def create_user(user: schemas.UserCreate):
-    fake_hashed_password = user.password + "notreallyhashed"
-    db_user = models.User(
-        email=user.email_address, hashed_password=fake_hashed_password
-    )
-    return()
+def get_password_hash(password):
+    return pwd_context.hash(password)
--- data/schemas.py
+++ data/schemas.py
@@ -1,15 +1,18 @@
 from pydantic import BaseModel
 
+# Schemas for Users
+
+# Common for all user interactions
 class UserBase(BaseModel):
     id: str
     email_address: str
     password: str
 
-
+# Nothing extra needed for Login
 class UserLogin(UserBase):
     pass
 
-
+# All single user data
 class User(UserBase):
     first_name: str
     last_name: str
